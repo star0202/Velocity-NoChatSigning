06d5f3c6f10b29531334ac5b03ff98acb788e675
William <will27528@gmail.com>
2023-01-28 13:52:15 +0000

    Remove old groovy gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val junitVersion: String by project.extra
 
 allprojects {
     group = "com.velocitypowered"
-    version = "3.2.0-SNAPSHOT"
+    version = "3.2.0-SNAPSHOT-nochatsigning"
 }
 
 subprojects {
