7d705517182beab7b955efc73d998d2af581c79d
William <will27528@gmail.com>
2023-01-28 17:19:39 +0000

    remove invalid cancel and sign checks

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/keyed/KeyedChatHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/keyed/KeyedChatHandler.java
@@ -26,7 +26,6 @@ import com.velocitypowered.proxy.protocol.MinecraftPacket;
 import com.velocitypowered.proxy.protocol.packet.chat.ChatQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
-import net.kyori.adventure.text.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -46,22 +45,6 @@ public class KeyedChatHandler implements
   @Override
   public Class<KeyedPlayerChat> packetClass() {
     return KeyedPlayerChat.class;
-  }
-
-  public static void invalidCancel(Logger logger, ConnectedPlayer player) {
-    logger.fatal("A plugin tried to cancel a signed chat message."
-        + " This is no longer possible in 1.19.1 and newer. "
-        + "Disconnecting player " + player.getUsername());
-    player.disconnect(Component.text("A proxy plugin caused an illegal protocol state. "
-        + "Contact your network administrator."));
-  }
-
-  public static void invalidChange(Logger logger, ConnectedPlayer player) {
-    logger.fatal("A plugin tried to change a signed chat message. "
-        + "This is no longer possible in 1.19.1 and newer. "
-        + "Disconnecting player " + player.getUsername());
-    player.disconnect(Component.text("A proxy plugin caused an illegal protocol state. "
-        + "Contact your network administrator."));
   }
 
   @Override
@@ -104,23 +87,12 @@ public class KeyedChatHandler implements
     assert playerKey != null;
     return pme -> {
       PlayerChatEvent.ChatResult chatResult = pme.getResult();
-      if (!chatResult.isAllowed()
-          && playerKey.getKeyRevision().compareTo(IdentifiedKey.Revision.LINKED_V2) >= 0) {
-        invalidCancel(logger, player);
-        return null;
-      }
 
       if (chatResult.getMessage().map(str -> !str.equals(packet.getMessage())).orElse(false)) {
-        if (playerKey.getKeyRevision().compareTo(IdentifiedKey.Revision.LINKED_V2) >= 0) {
-          // Bad, very bad.
-          invalidChange(logger, player);
-        } else {
-          logger.warn("A plugin changed a signed chat message. The server may not accept it.");
-          return player.getChatBuilderFactory().builder()
-              .message(chatResult.getMessage().get() /* always present at this point */)
-              .setTimestamp(packet.getExpiry())
-              .toServer();
-        }
+        return player.getChatBuilderFactory().builder()
+                .message(chatResult.getMessage().get() /* always present at this point */)
+                .setTimestamp(packet.getExpiry())
+                .toServer();
       }
       return packet;
     };
